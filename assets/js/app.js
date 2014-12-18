02a7bf3a341e59d20aded0da1f6640bd6cb2134a
raphael <rmlx22@gmail.com>
2014-12-18 10:24:12 +0100

    test hook 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
 App.socket.on('console',function(msg){
 	console.log(msg);
 });
-
-
 App.socket.on('get-user-videos',function(videos){
 	App.VideoCollections = new App.Collections.Videos(videos);
 	if(!App.Home.isDestroyed)App.Home.update();
